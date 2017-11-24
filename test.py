bc02b9c287d11901c82ae1cb769397fff934bf32
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-11-25 01:02:06 +0800

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ def hello(bot, update):
 update.message.reply_text(
 'Hello {}'.format(update.message.from_user.first_name))
 
-updater = Updater('YOUR TOKEN HERE')
+updater = Updater('415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg')
 
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('hello', hello))
