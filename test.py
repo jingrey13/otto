c4991a57789196d2e5bc77b3836a8208df2f44c4
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-11-25 01:01:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,50 +1,16 @@
-import json
-import requests  
+from telegram.ext import Updater, CommandHandler
 
-TOKEN = "415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg"
-URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+def start(bot, update):
+update.message.reply_text('Hello World!')
 
-def get_url(url):
-    response = requests.get(url)
-    content = response.content.decode("utf8")
-    return content
+def hello(bot, update):
+update.message.reply_text(
+'Hello {}'.format(update.message.from_user.first_name))
 
+updater = Updater('YOUR TOKEN HERE')
 
-def get_json_from_url(url):
-    content = get_url(url)
-    js = json.loads(content)
-    return js
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
 
-
-def get_updates():
-    url = URL + "getUpdates"
-    js = get_json_from_url(url)
-    return js
-
-
-def get_last_chat_id_and_text(updates):
-    num_updates = len(updates["result"])
-    last_update = num_updates - 1
-    text = updates["result"][last_update]["message"]["text"]
-    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
-    return (text, chat_id)
-
-
-def send_message(text, chat_id):
-    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
-    get_url(url)
-    
-
-def main():
-    last_textchat = (None, None)
-    while True:
-        get_url("https://api.telegram.org/bot415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg/sendMessage?chat_id=114698280&text=Hello")
-        text, chat = get_last_chat_id_and_text(get_updates())
-        if (text, chat) != last_textchat:
-            send_message(text, chat)
-            last_textchat = (text, chat)
-        time.sleep(0.5)
-
-
-if __name__ == '__main__':
-    main()
+updater.start_polling()
+updater.idle()
