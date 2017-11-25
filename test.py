6e3fce96b6c042a986c47a122ca0d17220043ffe
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-11-25 10:33:29 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,2 @@
-from telegram.ext import Updater, CommandHandler
-
-def start(bot, update):
-update.message.reply_text('Hello World!')
-
-def hello(bot, update):
-update.message.reply_text(
-'Hello {}'.format(update.message.from_user.first_name))
-
-updater = Updater('415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg')
-
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('hello', hello))
-
-updater.start_polling()
-updater.idle()
+import requests
+r=requests.get("https://api.telegram.org/bot415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg/sendMessage?chat_id=114698280&text=Hello123")
