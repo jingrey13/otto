fd0ed23b9db9fab3d4cc3915e3eb9593f0d8e80d
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-11-25 11:03:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,16 @@
 import requests
-r=requests.get("https://api.telegram.org/bot415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg/sendMessage?chat_id=114698280&text=Hello123")
+token="415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg"
+
+class BotHandler:
+  
+  def __init__(self, token):
+        self.token = token
+        self.api_url = "https://api.telegram.org/bot{}/".format(token)
+  def send_message(self, chat_id, text):
+			params = {'chat_id': chat_id, 'text': text}
+			method = 'sendMessage'
+			resp = requests.post(self.api_url + method, params)
+			return resp
+    
+mybot = BotHandler(token)
+mybot.send_message(114698280, 'Hello123')
