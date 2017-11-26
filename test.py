b7b79a4126bb1581b1e3410b39a550419ca4fd1f
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-11-27 00:21:53 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,13 @@
-import requests
-token="415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg"
+from telegram.ext import Updater
+updater = Updater(token='TOKEN')
+dispatcher = updater.dispatcher
+import logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
-class BotHandler:
-  
-  def __init__(self, token):
-        self.token = token
-        self.api_url = "https://api.telegram.org/bot{}/".format(token)
-  def send_message(self, chat_id, text):
-			params = {'chat_id': chat_id, 'text': text}
-			method = 'sendMessage'
-			resp = requests.post(self.api_url + method, params)
-			return resp
-    
-mybot = BotHandler(token)
-mybot.send_message('114698280', 'Hello123')
+def start(bot, update):
+	bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+from telegram.ext import CommandHandler
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
+updater.start_polling()
