55a9c10fe96e97124f7883531649709f431a530d
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-11-27 01:46:43 +0800

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -1,20 +1,44 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler, MessageHandler, Filters
-
-updater = Updater(token='415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg')
-dispatcher = updater.dispatcher
-
 import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+import os
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
 
 def start(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    update.effective_message.reply_text("Hi!")
+
+
 def echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+    update.effective_message.reply_text(update.effective_message.text)
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-echo_handler = MessageHandler(Filters.text, echo)
-dispatcher.add_handler(echo_handler)
+def error(bot, update, error):
+    logger.warning('Update "%s" caused error "%s"', update, error)
 
-updater.start_polling()
+
+if __name__ == "__main__":
+    # Set these variable to the appropriate values
+    TOKEN = "415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg"
+    NAME = "hidden-sea-92222"
+
+    # Port is given by Heroku
+    PORT = os.environ.get('PORT')
+
+    # Enable logging
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    # Set up the Updater
+    updater = Updater(TOKEN)
+    dp = updater.dispatcher
+    # Add handlers
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(error)
+
+    # Start the webhook
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook("https://{}.herokuapp.com/{}".format(NAME, TOKEN))
+    updater.idle()
