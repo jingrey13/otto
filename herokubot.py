6e6ec1255326b4ccddc2fb4991e351bedcfb053d
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-11-27 00:53:27 +0800

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -1,4 +1,7 @@
 from telegram.ext import Updater
+from telegram.ext import MessageHandler, Filters
+from telegram.ext import CommandHandler
+
 updater = Updater(token='415048379:AAHoccZk9RPZll17K2mroxTp3U7qMl9s3sg')
 dispatcher = updater.dispatcher
 import logging
@@ -6,8 +9,13 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 def start(bot, update):
 	bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+def echo(bot, update):
+        bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
-from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
+
 updater.start_polling()
