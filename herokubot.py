95b5f7353d25b1319bb3dac3603c0fbac6d0058e
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-11-27 21:19:57 +0800

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -17,7 +17,7 @@ def pingall(bot,update):
     chatid = update.message.chat.id
     update.effective_message.reply_text(chatid)
     name = update.message.from_user.first_name
-    update.effective_message.reply_text(name+" wants to play a game. Where is everybody?\n@cheekydeeky\n@Meowcolm\n@Reinaku\n@JeriKokHo\n@Nnavi92\n@pamelatay\n@Haoward\n@JinGrey13")
+    update.effective_message.reply_text(name+" wants to play a game. Where is everybody?\n@edddddddyyy\n@Meowcolm\n@Reinaku\n@JeriKokHo\n@Nnavi92\n@pamelatay\n@Haoward\n@JinGrey13")
 
 
 if __name__ == "__main__":
