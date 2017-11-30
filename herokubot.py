8f31cce0207ef0d2689c224a10fe7b32b15c0b88
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-11-30 10:46:07 +0800

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -7,6 +7,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 jinotto = 114698280
 secrethitler = -1001100171347
+inspiretime=0
 
 def start(bot, update):
     update.effective_message.reply_text("Hi!")
@@ -42,11 +43,19 @@ def pingall(bot,update):
         name = update.message.from_user.first_name
         bot.sendMessage(chat_id=update.message.chat_id, text=name+' wants to play a game. Where is everybody?\n@edddddyyyy\n@Meowcolm\n@Reinaku\n@JeriKokHo\n@Nnavi92\n@pamelatay\n@Haoward\n@JinGrey13')
 def inspire(bot,update):
-    url = "http://inspirobot.me/api?generate=true"
-    r = requests.get(url)
-    quote = r.text
-    bot.sendMessage(chat_id=update.message.chat_id, text=quote)
-        
+    if inspiretime == 0:
+        url = "http://inspirobot.me/api?generate=true"
+        r = requests.get(url)
+        quote = r.text
+        bot.sendMessage(chat_id=update.message.chat_id, text=quote)
+        inspiretime = 1
+        re
+    else:
+        bot.sendMessage(chat_id=update.message.chat_id, text="We are inspired enough for now. Please wait.")
+def resetinspire(bot, job):
+    inspiretime = 0
+def resettimer(bot, update, job_queue):
+    job_queue_run_once(resetinspire, 60, context=update.message.chat_id)
 
 if __name__ == "__main__":
     # Set these variable to the appropriate values
@@ -64,6 +73,7 @@ if __name__ == "__main__":
     # Set up the Updater
     updater = Updater(TOKEN)
     dp = updater.dispatcher
+    j = dp.job_queue
     # Add handlers
     dp.add_handler(CommandHandler('start', start))
     #dp.add_handler(MessageHandler(Filters.text, echo))
@@ -72,6 +82,7 @@ if __name__ == "__main__":
     dp.add_handler(MessageHandler(Filters.text, greeting))
     dp.add_handler(CommandHandler('pingall', pingall))
     dp.add_handler(CommandHandler('inspire', inspire))
+    dp.add_handler(CommandHandler('inspire', timerreset, pass_job_queue=True))
 
     # Start the webhook
     updater.start_webhook(listen="0.0.0.0",
