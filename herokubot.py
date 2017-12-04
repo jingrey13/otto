d448b3907984042662bccd9275f501777402c5f6
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-12-05 00:49:35 +0800

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -46,7 +46,7 @@ def pingall(bot,update):
         sublist = [s.replace('\n', '') for s in sublist]
         name = update.message.from_user.first_name
         bot.sendMessage(chat_id=update.message.chat_id, text=name+' wants to play a game. Where is everybody?')
-        bot.sendMessage(chat_id=update.message.chat_id, text=sublist[1])
+        bot.sendMessage(chat_id=update.message.chat_id, text=sublist[0]+sublist[1]+sublist[2])
         del sublist[:]
 def inspire(bot,update,job_queue):
     global inspiretime
