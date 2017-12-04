dc709e0cc727f6a4df7af54db7e58c59b3a7564a
jingrey13 <33866477+jingrey13@users.noreply.github.com>
2017-12-05 00:23:13 +0800

    Update herokubot.py

--- herokubot.py
+++ herokubot.py
@@ -43,6 +43,8 @@ def pingall(bot,update):
         sub_file = open("subscriberlist.txt", "r")
         sublist = sub_file.readlines()
         sub_file.close()
+        sublist.strip('"')
+        sublist.strip('\n')
         name = update.message.from_user.first_name
         bot.sendMessage(chat_id=update.message.chat_id, text=name+' wants to play a game. Where is everybody?')
         bot.sendMessage(chat_id=update.message.chat_id, text=sublist)
